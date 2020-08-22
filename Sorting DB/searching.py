b95d8420bacfc51df799d796a4e007d8d782bf63
Jazis <gulko46@gmail.com>
2020-08-22 12:04:06 +0300

    percent added

--- Sorting DB/searching.py
+++ Sorting DB/searching.py
@@ -2,7 +2,7 @@
 
 import pymysql
 import pymysql.cursors
-import os
+import os, sys
 import requests
 import time
 from threading import Thread
@@ -124,8 +124,20 @@ try:
             pass
         print('------------------------')
 
+    class temp_0():
+        counter = 0
+        summary = 0 
 
-    def func(qwe):
+    def func(qwe, file, file_size):
+        temp_0.counter = temp_0.counter + 1
+        if temp_0.counter == 10000:
+            temp_0.summary = 0
+            for i in range(len(os.listdir(os.path.abspath(__file__).replace(os.path.abspath(__file__).split('/')[-1], 'sorting/')))):
+                file =  os.path.abspath(__file__).replace(os.path.abspath(__file__).split('/')[-1], 'sorting/') + os.listdir(os.path.abspath(__file__).replace(os.path.abspath(__file__).split('/')[-1], 'sorting/'))[i]
+                temp_0.summary = temp_0.summary + os.path.getsize(file)
+            p = float(temp_0.summary)/float(file_size)*100
+            sys.stdout.write('\r' + '|-> ' + str(int(p)) + '%')
+            temp_0.counter = 0
         try:
             bkv = qwe[0] + qwe[1]
             bkv = bkv.lower()
@@ -147,22 +159,24 @@ try:
             threads1 = []
             threads0 = []
             base = base.replace('"', '').replace("'", "").replace(' ', '')
+            file_size = os.path.getsize(base)
             with open(base, 'r') as file:
                 try:
                     for line in file:
                         line = line.replace('\r', '')
                         try:
-                            if len(threads0) >= 200:
+                            if len(threads0) >= 500:
+                                # sys.stdout.write("\n")   
                                 time.sleep(0.1)
                                 threads0 = []
                                 threads1.append(line)
-                                thread2 = Thread(target=func, args=(line, ))
+                                thread2 = Thread(target=func, args=(line, file, file_size))
                                 thread2.start()
                             else:
                                 try:
                                     threads1 = []
                                     threads0.append(line)
-                                    thread1 = Thread(target=func, args=(line, ))
+                                    thread1 = Thread(target=func, args=(line, file, file_size))
                                     thread1.start()
                                 except RuntimeError:
                                     pass
@@ -172,10 +186,6 @@ try:
                     pass
 
     def base_sorting_struct():
-        try:
-            os.mkdir('sorting')
-        except OSError:
-            pass
         while True:
             base = input('BSE PLZ RETARD FR SRT ->')
             base = base.replace('"', '').replace("'", "").replace(' ', '')
